71cc2c14733f2c9af30694a844e6be0c34e8bb1b
siwachs <ssiwach.8888@gmail.com>
2024-04-01 16:29:32 +0530

    Add Clerk Auth

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,5 @@
+export default function layout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return <div>{children}</div>;
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <p>Authenticated User</p>
+    </main>
+  );
 }
