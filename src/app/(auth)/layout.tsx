aa6b33119b78444c6721082287a0a2933b505f81
siwachs <ssiwach.8888@gmail.com>
2024-04-02 11:43:30 +0530

    Integrate Authentication

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,5 +1,7 @@
-export default function layout({
+export default function AuthLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
-  return <div>{children}</div>;
+  return (
+    <div className="h-full flex items-center justify-center">{children}</div>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,3 @@
 export default function Home() {
-  return (
-    <main>
-      <p>Authenticated User</p>
-    </main>
-  );
+  return <main></main>;
 }
